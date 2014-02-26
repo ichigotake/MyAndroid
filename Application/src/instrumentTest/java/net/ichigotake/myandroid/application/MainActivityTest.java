f1a6fb14e24bf55d0d59fc33b628efed349a17a3
ichigotake <k.wisiiy@gmail.com>
2014-02-26 18:22:55 +0900

    add my usual application project template

---
+++ Application/src/instrumentTest/java/net/ichigotake/myandroid/application/MainActivityTest.java
@@ -0,0 +1,14 @@
+package net.ichigotake.myandroid.application;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    public void testRun() {
+        assertTrue(true);
+    }
+}
